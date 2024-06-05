da4db0bbb4ad858aada5fe63d9e067aad884af46
nisslyak <nissopodolyak@gmail.com>
2024-06-05 06:45:46 +0300

    Updating the white space

--- Solution/Solution/obj/Debug/net8.0/Solution.AssemblyInfo.cs
+++ Solution/Solution/obj/Debug/net8.0/Solution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6319d2e5b9d58abc1084b58715f569d40e4324")]
 [assembly: System.Reflection.AssemblyProductAttribute("Solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
